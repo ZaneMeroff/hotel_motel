316bc0f4349f18a61529989d97a563e6f55c1d2d
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-10 10:31:02 -0700

    Build search results funct

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,20 +1,26 @@
 import $ from 'jquery';
+import './index.js';
 
 const domUpdates = {
 
   displayCustomerWelcomeScreen: () => {
-    $('.h1-heading').text('Welcome, Trudie!')
+    $('.h1-heading').text('Howdy, Trudie!')
     $('.h2-heading').text('as of 01/09/20 you have spent $1,345.48')
   },
 
   logOutUser: $('.log-out-button').on('click', () => {
      location.reload();
-   }),
+  }),
 
-   // bookACabin: $('.book-a-cabin-button').on('click', () => {
-   //    $('.past-future-container').addClass('hidden');
-   // }),
-
+  displayCustomerSearchResults: $('.select-date-button').on('click', () => {
+     $('.available-res-container').removeClass('hidden');
+     $('.book-a-cabin-container').addClass('hidden');
+     $('.past-future-container').addClass('hidden');
+     $('.new-search-button').removeClass('hidden');
+     // hideOrShowElement('show', '.available-res-container');
+     // hideOrShowElement('hide', '.book-a-cabin-container');
+     // hideOrShowElement('hide', '.past-future-container');
+  }),
 
 
 
