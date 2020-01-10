c65a9f7cef89529ecb25e152fdf464fb4435de6e
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-09 20:24:34 -0700

    Build pastpresent box

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 
+const domUpdates = {
+
+  displayCustomerWelcomeScreen: () => {
+    $('.h1-heading').text('Welcome, Trudie!')
+    $('.h2-heading').text('as of 01/09/20 you have spent $1,345.48')
+  },
+
+  logOutUser: $('.log-out-button').on('click', () => {
+     location.reload();
+   }),
+
+   bookACabin: $('.book-a-cabin-button').on('click', () => {
+      $('.past-future-container').addClass('hidden');
+   }),
 
 
 
@@ -8,8 +22,8 @@ import $ from 'jquery';
 
 
 
+}
 
 
 
-
-export default 'domUpdates.js';
+export default domUpdates;
