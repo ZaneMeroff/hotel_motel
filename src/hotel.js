1a24a1e36fea87976465fb7d03be4a0982c107e4
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-13 02:27:07 -0700

    Add calculate todays bookings

--- src/hotel.js
+++ src/hotel.js
@@ -8,12 +8,29 @@ class Hotel {
     this.rooms = rooms;
     this.bookings = bookings;
     this.date = date;
-    this.roomsAvailableToday = []
+    this.roomsAvailableToday = [];
     this.totalOccupancy = 0;
   }
 
   calculateRoomsAvailableToday() {
-    
+
+  }
+
+  calculateTodaysBookings() {
+    let todaysBookings = this.bookings.filter(booking => {
+      return booking.date === this.date;
+    })
+    this.roomsAvailableToday.push(todaysBookings);
+  }
+
+  calculateTotalOccupancy() {
+  // filter bookings that match todays dates
+  // use that arrays length / 25
+  // then grab the decimal places and reassign to this.totalOccupancy
+  let todaysBookings = hotel.bookings.filter(booking => {
+    return booking.date === this.todaysDate;
+  })
+
   }
 
 
